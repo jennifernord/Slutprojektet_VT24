b4f3569ad566be1fd4cbfb8bc250e10f28cd264c
jennifer.nord <jennifer.nord@elev.ga.ntig.se>
2024-04-29 09:36:33 +0200

    .

--- src/Meny.java
+++ src/Meny.java
@@ -1,4 +1,5 @@
 //Låter användaren ta del av programmets funktioner
+
 import java.nio.channels.ScatteringByteChannel;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
@@ -9,38 +10,47 @@ public class Meny {
     public Meny() {
         MenuStart();
     }
+
     Scanner scan = new Scanner(System.in);
     ArrayList<RegularGuest> bookings = new ArrayList<>();
 
     //All kod för att användarmenyn ska fungera
     private void MenuStart() {
         while (true) {
-            System.out.println("1. Create booking\n" +
-                    "2. See all bookings\n" +
-                    "Enter option: ");
             try {
+                System.out.println("1. Create booking\n" +
+                        "2. See all bookings\n" +
+                        "3: quit\n" +
+                        "Enter option: ");
                 int option = scan.nextInt();
                 if (option == 1) {
-                        System.out.println("1. Regular guest\n" +
-                                "2. VIP guest\n" +
-                                "Choose type of guest: ");
-                        int typeGuest = scan.nextInt();
-                        if (typeGuest == 1) {
-                            newGuest();
-                        } else if (typeGuest == 2) {
-                            newVipGuest();
-                        } else {
-                            throw new Exception("Please try again, only option '1' and '2' is available: ");
-                        }break;
+                    System.out.println("1. Regular guest\n" +
+                            "2. VIP guest\n" +
+                            "Choose type of guest: ");
+                    int typeGuest = scan.nextInt();
+                    if (typeGuest == 1) {
+                        newGuest();
+                    } else if (typeGuest == 2) {
+                        newVipGuest();
+                    }
+
                 } else if (option == 2) {
                     for (RegularGuest guest : bookings) {
                         System.out.println(guest.name + " has phone number " + guest.phoneNr);
                     }
-                }else {
+                } else if (option == 3) {
+                    System.out.println("Quitting");
+                    break;
+                } else {
+
+                    //System.out.println("Else error");
                     throw new Exception("Please try again, only option '1' or '2' is available:");
-                }break;
+
+                }
+
             } catch (Exception e) {
                 scan.nextLine();
+                //System.out.println("catch error");
                 System.out.println("Please try again, only option '1' or '2' is available\n" +
                         "Only enter option with numbers: ");
             }
@@ -62,10 +72,10 @@ public class Meny {
                     "Your phone number is: " + phoneNr);
             VipGuest guest = new VipGuest(name, age, phoneNr);
             guest.addRoom();
-
             bookings.add(guest);
-        }catch (InputMismatchException e){
+        } catch (InputMismatchException e) {
             scan.nextLine();
+            //System.out.println("line 77 error");
             System.out.println("Error, please try again\n" +
                     "Only use numbers while entering phone number and age\n");
         }
@@ -73,24 +83,28 @@ public class Meny {
 
     //Metod för att skapa ny vanlig gäst
     private void newGuest() {
-        try {
-            System.out.println("Enter age: ");
-            int age = scan.nextInt();
-            System.out.println("Enter phone number: ");
-            int phoneNr = scan.nextInt();
-            scan.nextLine();
-            System.out.println("Enter name: ");
-            String name = scan.nextLine();
-            System.out.println("Your name is " + name + "\n" +
-                    "Your age is " + age + "\n" +
-                    "Your phone number is: " + phoneNr);
-            RegularGuest guest = new RegularGuest(name, age, phoneNr);
-            guest.addRoom();
-            bookings.add(guest);
-        } catch (InputMismatchException e) {
-            scan.nextLine();
-            System.out.println("Error, please try again\n" +
-                    "Only use numbers while entering phone number and age\n");
+        while (true) {
+            try {
+                System.out.println("Enter age: ");
+                int age = scan.nextInt();
+                System.out.println("Enter phone number: ");
+                int phoneNr = scan.nextInt();
+                scan.nextLine();
+                System.out.println("Enter name: ");
+                String name = scan.nextLine();
+                System.out.println("Your name is " + name + "\n" +
+                        "Your age is " + age + "\n" +
+                        "Your phone number is: " + phoneNr);
+                RegularGuest guest = new RegularGuest(name, age, phoneNr);
+                guest.addRoom();
+                bookings.add(guest);
+                break;
+            } catch (InputMismatchException e) {
+                scan.nextLine();
+                System.out.println("line 101 error");
+                System.out.println("Error, please try again\n" +
+                        "Only use numbers while entering phone number and age\n");
+            }
         }
     }
 }
--- src/RegularGuest.java
+++ src/RegularGuest.java
@@ -1,60 +1,63 @@
 //Klass som skapar en vanlig gäst
+
 import java.util.Scanner;
-public class RegularGuest extends Guest{
+
+public class RegularGuest extends Guest {
     Scanner scan = new Scanner(System.in);
 
     //Vanliga gästens attribut
-    protected RegularGuest(String name, int age, int phoneNr){
+    protected RegularGuest(String name, int age, int phoneNr) {
         super(name, age, phoneNr);
     }
 
     //Metod där den vanliga gästen kan äga ett rum
-    protected void addRoom(){
+    protected void addRoom() {
+        enterHotel();
+        System.out.println("1. Regular Room\n" +
+                "2. VIP Room\n" +
+                "Enter option here: ");
+        int option = scan.nextInt();
+        if (option == 1) {
+            try {
+                System.out.println("Enter number of beds needed: ");
+                int beds = scan.nextInt();
+                this.room = new RegularRoom(beds, hotel);
+                System.out.println("You have booked a room with " + beds + " beds");
+            } catch (Exception e) {
+                scan.nextLine();
+                System.out.println("Error beds");
+                System.out.println("Please try again, only option '1' or '2' is available\n" +
+                        "Only enter option with numbers: ");
+            }
+        }
+        if (option == 2) {
+            try {
+                System.out.println("Enter number of beds needed: ");
+                int beds = scan.nextInt();
+                System.out.println("Choose a privilege: \n" +
+                        "1. Breakfast in bed\n" +
+                        "2. Poolside view\n" +
+                        "3. Balcony\n" +
+                        "Enter option here: ");
+                int privilege = scan.nextInt();
+                System.out.println("You have chosen option " + privilege);
+                this.room = new VipRoom(beds, hotel, privilege);
+                System.out.println("You have booked a room with " + beds + " beds and privilege nr " + privilege);
+            } catch (Exception e) {
+                scan.nextLine();
+                System.out.println("Error vip");
+                System.out.println("Only enter option with numbers: ");
+            }
+        }
+    }
+
+    private void enterHotel() {
         System.out.println("Enter hotel you want to book the room in: ");
         String hotelName = scan.nextLine();
         Hotel hotel = new Hotel(hotelName);
-        try {
-            System.out.println("1. Regular Room\n" +
-                    "2. VIP Room\n" +
-                    "Enter option here: ");
-            int option = scan.nextInt();
-            if (option == 1){
-                try {
-                    System.out.println("Enter number of beds needed: ");
-                    int beds = scan.nextInt();
-                    this.room = new RegularRoom(beds, hotel);
-                    System.out.println("You have booked a room with " + beds + " beds");
-                }catch (Exception e){
-                    scan.nextLine();
-                    System.out.println("Please try again, only option '1' or '2' is available\n" +
-                            "Only enter option with numbers: ");
-                }
-            }
-            if (option == 2){
-                try {
-                    System.out.println("Enter number of beds needed: ");
-                    int beds = scan.nextInt();
-                    System.out.println("Choose a privilege: \n" +
-                            "1. Breakfast in bed\n" +
-                            "2. Poolside view\n" +
-                            "3. Balcony\n" +
-                            "Enter option here: ");
-                    int privilege = scan.nextInt();
-                    System.out.println("You have chosen option " + privilege);
-                    this.room = new VipRoom(beds, hotel, privilege);
-                    System.out.println("You have booked a room with " + beds + " beds and privilege nr " + privilege);
-                }catch (Exception e){
-                    scan.nextLine();
-                    System.out.println("Only enter option with numbers: ");
-                }
-            } else {
-                throw new Exception("Please try again, only option '1' or '2' is available");
-            }
-        }catch (Exception e){
-            scan.nextLine();
-            System.out.println("Please try again, only option '1' or '2' is available\n" +
-                    "Only enter option with numbers: ");
-        }
+    }
+
+    private void chooseRoom() {
 
     }
 }
